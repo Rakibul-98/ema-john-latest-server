bd37b993349d38d76ff14fae1fea5014cad80929
Rakibul-98 <rakibul.rupom2001@gmail.com>
2020-10-09 23:03:26 +0600

    resolving home page

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ app.use(cors());
 
 const port = 5000;
 
+app.get('/',(req,res)=>{
+    res.send("working smoothly...")
+})
+
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
